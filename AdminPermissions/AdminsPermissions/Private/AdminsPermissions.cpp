ca054903d01a447e9f5256de5a445ea5e4fffdf4
Pelayori <31128562+Pelayori@users.noreply.github.com>
2026-02-27 18:43:11 +0100

    [AP] Add discord webhook messages

--- AdminPermissions/AdminsPermissions/Private/AdminsPermissions.cpp
+++ AdminPermissions/AdminsPermissions/Private/AdminsPermissions.cpp
@@ -1,6 +1,11 @@
 #include "Permissions.h"
+#include "Requests.h"
 
+#include <ctime>
 #include <fstream>
+#include <unordered_map>
+
+#include <shellapi.h>
 
 #include "json.hpp"
 
@@ -17,6 +22,117 @@ std::string GetConfigPath()
 FString GetText(const std::string& str)
 {
 	return FString(API::Tools::Utf8Decode(config["Messages"].value(str, "No message")).c_str());
+}
+
+std::string GetTimestampString()
+{
+	std::time_t now = std::time(nullptr);
+	std::tm local_time{};
+	localtime_s(&local_time, &now);
+
+	char time_buffer[32]{};
+	std::strftime(time_buffer, sizeof(time_buffer), "%Y-%m-%d %H:%M:%S", &local_time);
+	return time_buffer;
+}
+
+std::string GetMapNameString()
+{
+	LPWSTR* argv;
+	int argc;
+	int i;
+	FString param(L"-serverkey=");
+	FString map_name = L"Unknown";
+
+	AShooterGameMode* game_mode = AsaApi::GetApiUtils().GetShooterGameMode();
+	if (game_mode)
+		game_mode->GetMapName(&map_name);
+
+	argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+	if (NULL != argv)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			FString arg(argv[i]);
+			if (arg.Contains(param))
+			{
+				if (arg.RemoveFromStart(param))
+				{
+					map_name = arg;
+					break;
+				}
+			}
+		}
+
+		LocalFree(argv);
+	}
+
+	return API::Tools::Utf8Encode(*map_name);
+}
+
+std::string GetLocationString(AShooterPlayerController* player_controller)
+{
+	if (!player_controller)
+		return "cheat spi 0 0 0";
+
+	const FVector loc = player_controller->GetLocation();
+	return fmt::format("cheat spi {:.2f} {:.2f} {:.2f}", loc.X, loc.Y, loc.Z);
+}
+
+void SendWebhook(AShooterPlayerController* player_controller, const FString& command)
+{
+	const auto discord = config.value("DiscordWebhook", nlohmann::json::object());
+	if (!discord.value("Enabled", false))
+		return;
+
+	const std::string webhook_url = discord.value("URL", "");
+	if (webhook_url.empty())
+	{
+		Log::GetLog()->warn("DiscordWebhook.Enabled is true but DiscordWebhook.URL is empty");
+		return;
+	}
+
+	const std::string webhook_template = discord.value(
+		"Message",
+		"[{timestamp}] {charactername} ({eos_id}) used /cheat {command} on {map} at {location}");
+
+	const FString character_name_f = AsaApi::IApiUtils::GetCharacterName(player_controller);
+	const std::string character_name = API::Tools::Utf8Encode(*character_name_f);
+	const std::string eos_id = player_controller ? player_controller->GetEOSId().ToString() : "";
+	const std::string timestamp = GetTimestampString();
+	const std::string map_name = GetMapNameString();
+	const std::string location = GetLocationString(player_controller);
+	const std::string command_text = API::Tools::Utf8Encode(*command);
+
+	std::string message_content;
+	try
+	{
+		message_content = fmt::format(
+			webhook_template,
+			fmt::arg("eos_id", eos_id),
+			fmt::arg("timestamp", timestamp),
+			fmt::arg("charactername", character_name),
+			fmt::arg("map", map_name),
+			fmt::arg("location", location),
+			fmt::arg("command", command_text));
+	}
+	catch (const std::exception& error)
+	{
+		Log::GetLog()->warn("Invalid DiscordWebhook.Message format: {}", error.what());
+		return;
+	}
+
+	nlohmann::json payload;
+	payload["content"] = message_content;
+
+	const std::string sender_name = discord.value("SenderName", "");
+	if (!sender_name.empty())
+		payload["username"] = sender_name;
+
+	API::Requests::Get().CreatePostRequest(
+		webhook_url,
+		[](bool, std::string, std::unordered_map<std::string, std::string>) {},
+		payload.dump(),
+		"application/json");
 }
 
 void ConsoleCommand(APlayerController* player, FString command)
@@ -48,16 +164,18 @@ void OnChatMessage(AShooterPlayerController* player_controller, FString* message
 		if (!message->RemoveFromStart("/cheat "))
 			return;
 
-		ConsoleCommand(player_controller, *message);
+		const FString command = *message;
+		ConsoleCommand(player_controller, command);
 
 		FString log = FString::Format(*GetText("LogMsg"), *AsaApi::IApiUtils::GetCharacterName(player_controller),
-			**message);
+			*command);
 
 		const bool print_to_chat = config["PrintToChat"];
 		if (print_to_chat)
 			AsaApi::GetApiUtils().SendServerMessageToAll(FColorList::Yellow, *log);
 
 		AsaApi::GetApiUtils().GetShooterGameMode()->PrintToGameplayLog(log);
+		SendWebhook(player_controller, command);
 	}
 	else
 	{
